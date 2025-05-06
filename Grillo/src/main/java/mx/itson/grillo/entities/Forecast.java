7560a6f326a0ce371749c0f3c65a39d4d313c76e
natyv <natyv@10.232.10.220>
2025-05-06 10:13:01 -0700

    Se agregaron mensajes

---
+++ Grillo/src/main/java/mx/itson/grillo/entities/Forecast.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.grillo.entities;
+
+import java.util.Date;
+
+/**
+ *
+ * @author natyv
+ */
+public class Forecast {
+    
+    private Date day;
+    private int maxTemperatura;
+    private int minTemperatura;
+    
+    
+}
